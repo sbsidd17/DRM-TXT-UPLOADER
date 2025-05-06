1c23edecb7689e7718f56853075871325281551d
Shuaib Siddiqui <122801841+sbsidd17@users.noreply.github.com>
2025-05-07 01:20:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 API_ID = os.environ.get("API_ID", "8087060") #Replace With your api id
 API_HASH = os.environ.get("API_HASH", "ead24e1abe37e9a80b16ef0f468350b6") #Replace With your api hash
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "2087326318:AAGRcDp10n62WWmjZN4njTKQXf5rVOKZbmg") #Replace With your bot token
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7439448092:AAG3GoCNTv-1d4IYP8Imny-wP_Uyyalnw6g") #Replace With your bot token
